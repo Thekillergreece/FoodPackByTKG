61be2a54156bb9ee7a539a8bdf95d420b7c74ffb
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2023-10-13 23:18:54 -0300

    Update PotTexture.cs

--- VisualStudio/TKG/PotTexture.cs
+++ VisualStudio/TKG/PotTexture.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-using System.Collections.Generic;
-using UnityEngine;
-using MelonLoader;
-using Il2Cpp;
-
-namespace TKG
+﻿namespace TKG
 {
     internal class PotTexture
     {
