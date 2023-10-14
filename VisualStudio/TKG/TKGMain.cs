278f5163ef8d782462b2c9d4cee44fb4a4a20588
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2023-10-13 23:12:24 -0300

    remove unused method

--- VisualStudio/TKG/TKGMain.cs
+++ VisualStudio/TKG/TKGMain.cs
@@ -7,11 +7,6 @@ namespace TKG
 {
     internal sealed class TKGMain : MelonMod
     {
-        public override void OnInitializeMelon()
-        {
-            //Any initialization code goes here.
-            //This method can be deleted if no initialization is required.
-        }
         public static bool loadedCookingTex;
         private static List<string> cookableGear = new List<string>();
         public static Material vanillaLiquidMaterial;
