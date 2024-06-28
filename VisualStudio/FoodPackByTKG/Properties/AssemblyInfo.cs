ac55c3e9f98d0f56824eac483456e8a07d6e903b
Thekillergreece <sakislkg@gmail.com>
2024-06-28 10:38:17 +0300

    fixed vs

---
+++ VisualStudio/FoodPackByTKG/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using MelonLoader;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using static UnityEngine.RemoteConfigSettingsHelper;
+
+//This is a C# comment. Comments have no impact on compilation.
+
+[assembly: AssemblyTitle("FoodPack")]
+[assembly: AssemblyCopyright($"Created by Inarissa & Thekillergreece")]
+
+[assembly: AssemblyVersion("1.6")]
+[assembly: AssemblyFileVersion("1.6")]
+[assembly: MelonInfo(typeof(TKG.TKGMain), "FoodPack", "1.3", "Inarissa & Thekillergreece")]
+
+//This tells MelonLoader that the mod is only for The Long Dark.
+[assembly: MelonGame("Hinterland", "TheLongDark")]
---
+++ VisualStudio/FoodPackByTKG/TKG/PotTexture.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using MelonLoader;
+using Il2Cpp;
+
+namespace TKG
+{
+    internal class PotTexture
+    {
+        // pot texture
+
+    }
+
+}
