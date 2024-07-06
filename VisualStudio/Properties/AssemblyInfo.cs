224870e72cfa77716b91bc3c7fd42bcceabaae1f
Thekillergreece <sakislkg@gmail.com>
2024-07-06 14:40:21 +0300

    edited

--- VisualStudio/Properties/AssemblyInfo.cs
+++ VisualStudio/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using static UnityEngine.RemoteConfigSettingsHelper;
 //This is a C# comment. Comments have no impact on compilation.
 
 [assembly: AssemblyTitle("FoodPack")]
-[assembly: AssemblyCopyright($"Created by Inarissa & Thekillergreece")]
+[assembly: AssemblyCopyright($"Created by Thekillergreece & Kaiusername")]
 
-[assembly: AssemblyVersion("1.6")]
-[assembly: AssemblyFileVersion("1.6")]
-[assembly: MelonInfo(typeof(TKG.TKGMain), "FoodPack", "1.3", "Inarissa & Thekillergreece")]
+[assembly: AssemblyVersion("5.5")]
+[assembly: AssemblyFileVersion("5.5")]
+[assembly: MelonInfo(typeof(TKG.TKGMain), "FoodPack", "5.5", "Thekillergreece & Kaiusername")]
 
 //This tells MelonLoader that the mod is only for The Long Dark.
 [assembly: MelonGame("Hinterland", "TheLongDark")]
